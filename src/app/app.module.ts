4ee3e6a2eced180f1b5fb56c7773d3206d67c42a
eha <ehassah@gmail.com>
2018-09-18 10:58:47 -0700

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { EditRecipeComponent } from './edit-recipe/edit-recipe.component';
+import { RecipeDescriptionComponent } from './recipe-description/recipe-description.component';
+
+const routes: Routes =[
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {path: 'contact', component: ContactComponent},
+  { path: 'add-recipe', component: AddRecipeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContactComponent,
+    FooterComponent,
+    AddRecipeComponent,
+    EditRecipeComponent,
+    RecipeDescriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
